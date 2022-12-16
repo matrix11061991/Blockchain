dbafdaa9523d71138f2f04174d92dd6791e328ce
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2022-12-16 07:07:29 +0200

    Update Blockchain.py

--- Blockchain.py
+++ Blockchain.py
@@ -1,54 +1,26 @@
-# main.py file
-
-"""
-
-A simple Blockchain in Python
-
-"""
 class Block:
+  def __init__(self, data, previous_hash):
+    self.data = data
+    self.previous_hash = previous_hash
+    self.hash = self.calc_hash()
 
-    def __init__(self, data, previous_hash):
-
-        self.data = data
-
-        self.previous_hash = previous_hash
-
-        self.hash = self.calculate_hash()
-
-        
-
-    def calculate_hash(self):
-
-        # Utiliser un algorithme de hachage pour calculer le hash du bloc
-
-        pass
+  def calc_hash(self):
+    sha = hashlib.sha256()
+    hash_str = self.data.encode('utf-8')
+    sha.update(hash_str)
+    return sha.hexdigest()
 
 class Blockchain:
+  def __init__(self):
+    self.chain = []
 
-    def __init__(self):
+  def add_block(self, data):
+    self.chain.append(Block(data, self.chain[-1].hash if len(self.chain) > 0 else None))
 
-        self.chain = [self.create_genesis_block()]
-
-        
-
-    def create_genesis_block(self):
-
-        # Créer le premier bloc de la chaîne
-
-        pass
-
-    
-
-    def get_previous_block(self):
-
-        return self.chain[-1]
-
-    
-
-    def add_block(self, data):
-
-        previous_block = self.get_previous_block()
-
-        new_block = Block(data, previous_block.hash)
-
-        self.chain.append(new_block)
+  def is_valid(self):
+    for i in range(1, len(self.chain)):
+      if self.chain[i].hash != self.chain[i].calc_hash():
+        return False
+      if self.chain[i].previous_hash != self.chain[i-1].hash:
+        return False
+    return True
