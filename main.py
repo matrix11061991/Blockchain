a72924bd7fb7b3b4ab232381b801bde9daeb031e
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2022-12-16 07:06:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
-# main.py file
+class Block:
+  def __init__(self, index, timestamp, data, previous_hash):
+    self.index = index
+    self.timestamp = timestamp
+    self.data = data
+    self.previous_hash = previous_hash
+    self.hash = self.calc_hash()
+  
+  def calc_hash(self):
+    sha = hashlib.sha256()
+    hash_str = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}"
+    sha.update(hash_str.encode('utf-8'))
+    return sha.hexdigest()
 
-"""
+class Blockchain:
+  def __init__(self):
+    self.chain = [self.create_genesis_block()]
+  
+  def create_genesis_block(self):
+    return Block(0, datetime.datetime.now(), "Genesis Block", "0")
+  
+  def get_last_block(self):
+    return self.chain[-1]
+  
+  def add_block(self, new_block):
+    new_block.previous_hash = self.get_last_block().hash
+    new_block.hash = new_block.calc_hash()
+    self.chain.append(new_block)
 
-A simple Blockchain in Python
+blockchain = Blockchain()
 
-"""
-import numpy as np
+blockchain.add_block(Block(1, datetime.datetime.now(), "Some data here", "0"))
+blockchain.add_block(Block(2, datetime.datetime.now(), "More data here", "0"))
+blockchain.add_block(Block(3, datetime.datetime.now(), "Even more data here", "0"))
 
-def navier_stokes(u, v, p, dx, dy, dt, viscosity):
-    un = np.zeros_like(u)
-    vn = np.zeros_like(v)
-    
-    # calcul des termes de diffusion
-    u_diff = (viscosity * dt / dx**2) * (np.roll(u, -1, axis=1) - 2 * u + np.roll(u, 1, axis=1))
-    v_diff = (viscosity * dt / dy**2) * (np.roll(v, -1, axis=0) - 2 * v + np.roll(v, 1, axis=0))
-    
-    # calcul des termes de convection
-    u_convect = u * (np.roll(u, -1, axis=1) - np.roll(u, 1, axis=1)) + v * (np.roll(u, -1, axis=0) - np.roll(u, 1, axis=0))
-    v_convect = u * (np.roll(v, -1, axis=1) - np.roll(v, 1, axis=1)) + v * (np.roll(v, -1, axis=0) - np.roll(v, 1, axis=0))
-    
-    # calcul de la pression
-    p_term = -(dx**2 / dt) * (np.roll(u, -1, axis=1) - np.roll(u, 1, axis=1))
-    q_term = -(dy**2 / dt) * (np.roll(v, -1, axis=0) - np.roll(v, 1, axis=0))
-    p = (p_term + q_term) / (2 * (dx**2 + dy**2))
-    
-    # calcul des vitesses pour le prochain pas de temps
-    un = u + dt * (-u_convect + u_diff)
-    vn = v + dt * (-v_convect + v_diff)
-    
-    return un, vn, p
+print(blockchain.chain)
